a6123b16b84ca6e3d693af077aba8d65e411d4e2
naoki yokoyama <naoki.yokoyama@eemovel.com>
2022-12-29 10:40:58 -0300

    modify app

--- mathfin.py
+++ mathfin.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 class Price:
   def __init__(self, juros, principal, parcelas):
+    '''
+    -> Calculo de prestacao connstante
+       :juros -> insira o valor do juros \n   Ex: 2% input apenas o valor de 2\n
+       :parcelas -> numero inteiro de parcelas\n
+       :principal -> valor de entrada
+    '''
     self.juros = juros
     self.principal = principal
     self.parcelas = parcelas
@@ -15,9 +21,11 @@ class Price:
     return juros_convertido / formula
 
   def valor_prestacao(self):
+    '''Retorna o valor da prestação'''
     return round (self.coeficiente() * self.principal,2)
 
   def dataframe(self):
+    ''' -> Retorna o dataframe da tabela PRICE'''
     n = self.parcelas 
     prestacao = self.valor_prestacao()
     juros = self.juros / 100
@@ -45,11 +53,18 @@ class Price:
 
 class Sac:
   def __init__(self, juros, principal, parcelas):
+    '''
+    -> Calculo de armotização constante
+       :juros -> insira o valor do juros \n   Ex: 2% input apenas o valor de 2\n
+       :parcelas -> numero inteiro de parcelas\n
+       :principal -> valor de entrada  
+    '''
     self.juros = juros
     self.principal = principal
     self.parcelas = parcelas
   
   def dataframe(self):
+    '''-> Retorna o dataframe da tabela SAC'''
     n = self.parcelas 
     vp = self.principal
     armotizacao = vp/n
@@ -79,5 +94,56 @@ class Sac:
       pg = self.dataframe()['Juros'].values
       return round(pg[1:].sum(),2)
 
+class Hp12c:
+  def __init__(self, juros=0, parcelas=0, pv=0, fv=0, pmt=0):
+    '''
+      -> Alguns calculos de matematica financeira
+      :juros -> insira o valor do juros \n   Ex: 2% input apenas o valor de 2\n
+       :parcelas -> numero inteiro de parcelas \n
+       :pv -> valor presente (valor da entrada) \n
+       :fv -> valor futuro (valor final) \n
+       :pmt -> valor da prestação]
+    '''
+    self.juros=juros
+    self.parcelas = parcelas
+    self.pv = pv
+    self.fv = fv
+    self.pmt = pmt
+  
+  def coeficiente(self):
+    parcelas = self.parcelas
+    juros = self.juros
+    juros_convertido = juros/100 
+    formula = 1-(1+juros_convertido)**-self.parcelas
+    return juros_convertido / formula
+
+  def value_future(self):
+    '''calcula o valor futuro'''
+    pv = self.pv
+    i = self.juros/100
+    n = self.parcelas
+    return round(pv * (1+i)**n,2)
+
+  def present_value(self):
+    '''calcula o valor presente'''
+    fv = self.fv
+    i = self.juros/100
+    n = self.parcelas
+    return round(fv / (1+i)**n,2)    
+
+  def payment(self):
+    '''calcula a prestação'''
+    coeficiente = self.coeficiente()    
+    pv = self.pv
+    return round(coeficiente * pv, 2)
+
+  def taxa_juros(self):
+    '''calcula a taxa de juros efetiva'''
+    n = self.parcelas
+    fv = self.fv
+    pv = self.pv
+    i = (fv/pv)**(1/n) - 1
+    return round (i*100,2)      
+
 if __name__ == "__main__":
-    print ('tabelas de SAC e Price')
+    print ('tabelas de SAC // Price // Hp12c')
